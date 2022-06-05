94ff1ea9d078286091d612b0dd84500b3efb423a
Shahabas Abdul Hameed <96893016+S488U@users.noreply.github.com>
2022-06-05 20:42:30 +0530

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,35 @@
+
+//add active class in selected list item 
+ let list = document.querySelectorAll('.list');
+ for (let i=0; i<list.lenghth; i++){
+    list[i].onclick = function(){
+         let j = 0;
+         while(j < list.lenghth){
+             list[j++].className = 'list'; 
+         }
+         list[i].className = 'list active';
+     }
+ }
+ 
+ function openPage(){
+    var x = document.getElementById("search").value
+         if (x === "shafna") {
+             window.open("/second.html");
+         }
+}
+
+
+function myFunction(){
+    alert("Why are you clicked?");
+}
+function me(){
+         alert("You are successfully loged in.");
+     }
+
+
+
+function clickf(){
+    var a = 20;
+    var b = 30;
+    document.write(a + b);
+}
